ea46ff491c14624df0babd1d87b53f269f732fac
Luis Kupferberg <lkupferberg@overactive.com>
2020-07-09 05:25:47 -0400

    Adding Beer Repository Closes #6.

---
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.msscbeerservice.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
+ * @created 2020-07-09 05:23
+ */
+public interface BeerRepository extends PagingAndSortingRepository {
+}
