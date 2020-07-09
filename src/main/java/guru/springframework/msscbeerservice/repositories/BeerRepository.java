c7ac3e045fabf3214639801bf8e090b93d7241a0
Luis Kupferberg <lkupferberg@overactive.com>
2020-07-09 05:28:35 -0400

    Fixing Beer Repository #6.

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbeerservice.repositories;
 
+import guru.springframework.msscbeerservice.domain.Beer;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
 
 /**
  * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
  * @created 2020-07-09 05:23
  */
-public interface BeerRepository extends PagingAndSortingRepository {
-}
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {}
