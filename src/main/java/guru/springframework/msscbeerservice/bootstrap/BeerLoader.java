42ca465a002c3825c65f40ad6bb1c90b6c6199b7
Luis Kupferberg <lkupferberg@overactive.com>
2020-07-19 22:36:25 -0400

    Adding the initialization custom data. closes #16.

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -1,17 +1,13 @@
 package guru.springframework.msscbeerservice.bootstrap;
 
-import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
 
 /**
  * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
  * @created 2020-07-09 20:59
  */
-@Component
+//@Component
 public class BeerLoader implements CommandLineRunner {
 
   public static final String BEER_1_UPC = "0631234200036";
@@ -26,40 +22,7 @@ public class BeerLoader implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    loadBeerObjects();
   }
 
-  private void loadBeerObjects() {
-    if (beerRepository.count() == 0) {
-      beerRepository.save(
-          Beer.builder()
-              .beerName("Mango Bobs")
-              .beerStyle("IPA")
-              .quantityToBrew(200)
-              .minOnHand(12)
-              .upc(BEER_1_UPC)
-              .price(new BigDecimal("12.95"))
-              .build());
 
-      beerRepository.save(
-          Beer.builder()
-              .beerName("Galaxy Cat")
-              .beerStyle("PALE_ALE")
-              .quantityToBrew(200)
-              .minOnHand(12)
-              .upc(BEER_2_UPC)
-              .price(new BigDecimal("11.95"))
-              .build());
-
-      beerRepository.save(
-              Beer.builder()
-                      .beerName("The Hammers on the Bar")
-                      .beerStyle("PALE_ALE")
-                      .quantityToBrew(200)
-                      .minOnHand(12)
-                      .upc(BEER_3_UPC)
-                      .price(new BigDecimal("11.95"))
-                      .build());
-    }
-  }
 }
